78b49135dff34ff776f3d4dacd1081268bb11590
Akshay <akshaynighot73@gmail.com>
2025-02-03 21:28:34 +0530

    Add Application class and initialize koin di

---
+++ app/src/main/java/com/absut/currencyconverter/CurrencyConverterApp.kt
@@ -0,0 +1,23 @@
+package com.absut.currencyconverter
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+class CurrencyConverterApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@CurrencyConverterApp)
+            modules(appModules)
+        }
+    }
+
+}
+
+val appModules = module {
+    //todo define dependencies here
+
+}
