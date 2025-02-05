dd244a847fe510185da954080d953299a57de267
Akshay <akshaynighot73@gmail.com>
2025-02-05 19:47:14 +0530

    update Theme.kt

--- app/src/main/java/com/absut/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/absut/currencyconverter/MainActivity.kt
@@ -20,28 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             CurrencyConverterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Text(modifier = Modifier.padding(innerPadding), text = "Hello, World!")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CurrencyConverterTheme {
-        Greeting("Android")
-    }
-}
