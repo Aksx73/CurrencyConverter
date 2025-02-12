0636851d5a69c2d4fa6303c10d2672d4f9b67c92
Akshay <akshaynighot73@gmail.com>
2025-02-12 11:27:00 +0530

    setup repository interface and impl

--- app/src/main/java/com/absut/currencyconverter/data/repository/CurrencyRepositoryImpl.kt
+++ app/src/main/java/com/absut/currencyconverter/data/repository/CurrencyRepositoryImpl.kt
@@ -1,15 +1,19 @@
 package com.absut.currencyconverter.data.repository
 
+import com.absut.currencyconverter.data.local.CurrencyDao
 import com.absut.currencyconverter.data.mapper.toCurrencies
 import com.absut.currencyconverter.data.mapper.toCurrencyRates
 import com.absut.currencyconverter.data.remote.ApiService
 import com.absut.currencyconverter.domain.model.Currencies
+import com.absut.currencyconverter.domain.model.Currency
 import com.absut.currencyconverter.domain.model.Rates
 import com.absut.currencyconverter.domain.repository.CurrencyRepository
 import com.bjsindia.bjscommunity.data.remote.NetworkResult
+import kotlinx.coroutines.flow.Flow
 
 class CurrencyRepositoryImpl(
-	private val api: ApiService
+	private val api: ApiService,
+	private val dao: CurrencyDao
 ) : CurrencyRepository {
 
 	override suspend fun getCurrencies(): NetworkResult<Currencies> {
@@ -30,4 +34,19 @@ class CurrencyRepositoryImpl(
 		}
 	}
 
+	override fun getLocalCurrencies(): Flow<List<Currency>> {
+		return dao.getCurrencies()
+	}
+
+	override suspend fun getLocalRates(): Rates {
+		return dao.getRatesForEUR()
+	}
+
+	override suspend fun insertCurrencies(currencies: List<Currency>) {
+		return dao.insertCurrencies(currencies)
+	}
+
+	override suspend fun insertRates(rates: Rates) {
+		return dao.insertRates(rates)
+	}
 }
--- app/src/main/java/com/absut/currencyconverter/domain/repository/CurrencyRepository.kt
+++ app/src/main/java/com/absut/currencyconverter/domain/repository/CurrencyRepository.kt
@@ -1,12 +1,22 @@
 package com.absut.currencyconverter.domain.repository
 
 import com.absut.currencyconverter.domain.model.Currencies
+import com.absut.currencyconverter.domain.model.Currency
 import com.absut.currencyconverter.domain.model.Rates
 import com.bjsindia.bjscommunity.data.remote.NetworkResult
+import kotlinx.coroutines.flow.Flow
 
 interface CurrencyRepository {
 
 	suspend fun getCurrencies(): NetworkResult<Currencies>
 
 	suspend fun getRatesForEUR(): NetworkResult<Rates>
+
+	fun getLocalCurrencies(): Flow<List<Currency>>
+
+	suspend fun getLocalRates(): Rates
+
+	suspend fun insertCurrencies(currencies: List<Currency>)
+
+	suspend fun insertRates(rates: Rates)
 }
