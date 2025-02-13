752861a2d636c30f5da6e1f1b0807e6de5201092
Akshay <akshaynighot73@gmail.com>
2025-02-13 17:12:15 +0530

    Add @Keep annotation to models

--- app/src/main/java/com/absut/currencyconverter/domain/model/Currencies.kt
+++ app/src/main/java/com/absut/currencyconverter/domain/model/Currencies.kt
@@ -1,18 +1,24 @@
 package com.absut.currencyconverter.domain.model
 
+import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverter
 import androidx.room.TypeConverters
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 
 //@TypeConverters(CurrencyListConverter::class)
+@Keep
+@Serializable
 data class Currencies(
 	val currency: List<Currency>
 )
 
+@Keep
+@Serializable
 @Entity(tableName = "currency")
 data class Currency(
 	@PrimaryKey val code: String,
--- app/src/main/java/com/absut/currencyconverter/domain/model/CurrencyRates.kt
+++ app/src/main/java/com/absut/currencyconverter/domain/model/CurrencyRates.kt
@@ -1,12 +1,16 @@
 package com.absut.currencyconverter.domain.model
 
+import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverter
 import androidx.room.TypeConverters
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
+@Keep
+@Serializable
 @Entity(tableName = "eu_rates")
 @TypeConverters(CurrencyRateConverter::class)
 data class Rates(
@@ -15,6 +19,8 @@ data class Rates(
 	val rates: List<CurrencyRate>
 )
 
+@Keep
+@Serializable
 data class CurrencyRate(
 	val code: String,
 	val value: Double,
