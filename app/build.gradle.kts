59e8caab88c1eeecc566cf5e1f1438e8d5975f5f
Akshay <akshaynighot73@gmail.com>
2025-03-06 09:21:12 +0530

    add compose navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+
     implementation(libs.bundles.ktor)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.bundles.room)
