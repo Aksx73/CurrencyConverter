7d718a269e0ed2c7915bba6119bd64ec8f0c122f
Akshay <akshaynighot73@gmail.com>
2025-03-07 09:31:45 +0530

    add navigation composable

--- app/src/main/java/com/absut/currencyconverter/ui/views/Navigation.kt
+++ app/src/main/java/com/absut/currencyconverter/ui/views/Navigation.kt
@@ -1,21 +1,19 @@
 package com.absut.currencyconverter.ui.views
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.absut.currencyconverter.ui.viewmodel.MainViewModel
 
 @Composable
-fun Navigation(modifier: Modifier = Modifier) {
-	val navController = rememberNavController()
-
+fun AppNavHost(navController: NavHostController,viewModel: MainViewModel) {
 	NavHost(navController = navController, startDestination = Routes.HomeScreen.route) {
 		composable(route = Routes.HomeScreen.route) {
-			HomeScreen(navController = navController)
+			HomeScreen(navController = navController,viewModel = viewModel)
 		}
 		composable(route = Routes.CurrencyListScreen.route) {
-			CurrencyListScreen()
+			CurrencyListScreen(viewModel = viewModel)
 		}
 	}
 }
